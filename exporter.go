9e7f30adaf9877e3515b8493d09224a3295b1939
grishberg <grishberg@ya.com>
2019-12-19 08:56:40 +0300

    initial commit

---
+++ exporter.go
@@ -0,0 +1,70 @@
+// exporter.go tries to download videos from yandes.disk
+//
+// usage:
+//   > go get github.com/Grishberg/yandex-disk-restapi-go
+//   > go run exporter.go -token=access_token
+//
+//   You can find an access_token for your app at https://oauth.yandex.ru
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/Grishberg/yandex-disk-restapi-go/src"
+	"os"
+)
+
+func main() {
+	var accessToken string
+	flag.StringVar(&accessToken, "token", "", "Access Token")
+
+	if accessToken == "" {
+		accessToken = os.Getenv("YADISK_ACCESS_TOKEN")
+	}
+
+	mediaTypeImage := src.MediaType{}
+	v := mediaTypeImage.Video()
+	mediaTypes := []src.MediaType{*v}
+
+	flag.Parse()
+
+	if accessToken == "" {
+		fmt.Println("\nPlease provide an access_token, one can be found at https://oauth.yandex.ru")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	client := src.NewClient(accessToken)
+
+	fmt.Printf("Fetching flat file list ...\n")
+	var offset uint32 = 1
+	for i := 0; i < 30; i++ {
+		fmt.Println("read offset: ", offset)
+		readed := getFlatFileListWithOffset(client, mediaTypes, offset)
+		if readed == 0 {
+			break
+		}
+		offset += readed
+	}
+}
+
+func getFlatFileListWithOffset(client *src.Client, mediaTypes []src.MediaType, offset uint32) uint32 {
+	options := src.FlatFileListRequestOptions{Media_type: mediaTypes, Offset: &offset}
+	info, err := client.NewFlatFileListRequest(options).Exec()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for i, v := range info.Items {
+		fmt.Println(i, v.Name, v.Path)
+	}
+	if info.Limit != nil {
+		fmt.Printf("\tLimit: %d\n", info.Limit)
+	}
+	if info.Offset != nil {
+		fmt.Printf("\tOffset: %d\n", *info.Offset)
+	}
+	return uint32(len(info.Items))
+}
